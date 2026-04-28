af6d9367f9aa79b1c07518d62298f5fd855e39bf
fardinfaruk-bd <fardinfaruktofu990@gmail.com>
2026-04-28 23:38:26 +0600

    project setup and add Daisy ui, group routing

---
+++ src/app/(main)/news/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsDetailsPage = () => {
+    return (
+        <div>
+            <h2 className="text-3xl font-bold  text-red-500">This is news details page</h2>
+        </div>
+    );
+};
+
+export default NewsDetailsPage;
---
+++ src/app/(main)/page.js
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h2 className="text-3xl font-bold underline text-red-500">Homepage</h2>
+  );
+}
